96e1b382a764f7e78202129c23437c947fe341c1
WillianZamalloa <WillianZamalloa@users.noreply.github.com>
2015-07-03 18:47:39 -0500

    tercer commit

--- random.js
+++ random.js
@@ -1,6 +1,6 @@
 //Math.random() devuelve numero aleatorio entre 0 y 1
 var numero = Math.random();
-
+//Inicio la operacion if
 if (numero <= 0.5) {
   console.log('n' + numero + 'MENOR que 0.5 n');
 } else {
