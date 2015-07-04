53c8b234397bb354bfbe6b2ebc78f65897b6d404
WillianZamalloa <WillianZamalloa@users.noreply.github.com>
2015-07-03 20:10:19 -0500

    Primera Version

--- random.js
+++ random.js
@@ -2,7 +2,7 @@
 var numero = Math.random();
 //Inicio la operacion if
 if (numero <= 0.5) {
-  console.log('n' + numero + 'MENOR que 0.5 n');
+  console.log('n' + numero + 'MENOR que 0.5 n');//menor
 } else {
-  console.log('n' + numero + 'MAYOR que 0.5 n');
+  console.log('n' + numero + 'MAYOR que 0.5 n');//mayor
 }
